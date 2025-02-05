7e39afe129b4ea21076f4abf2fad04d0581cd2b4
Levi-McKenna <lmckenna090@gmail.com>
2025-02-04 20:04:42 -0500

    line overflow moves to the next line in an inline fashion

--- dist/resizeTermInput.js
+++ dist/resizeTermInput.js
@@ -1,12 +1,34 @@
 "use strict";
-function alterTextAreaSize() {
-    document.querySelectorAll("textarea").forEach((textarea) => {
-        textarea.style.height = textarea.scrollHeight + "px";
-        textarea.style.overflowY = "hidden";
-        textarea.addEventListener("input", function () {
+function alterTextHeight() {
+    const inputLine = document.getElementById("focus-input");
+    if (inputLine instanceof HTMLTextAreaElement) {
+        inputLine.style.height = inputLine.scrollHeight + "px";
+        inputLine.addEventListener("input", function () {
             this.style.height = "auto";
             this.style.height = this.scrollHeight + "px";
-            console.info("helpp");
         });
-    });
+    }
+}
+function terminalNewLine() {
+    const commandLine = document.getElementById("terminal--line"); // parent container
+    const mainLineInput = document.getElementById("focus-input"); // first input line
+    if (mainLineInput instanceof HTMLTextAreaElement) {
+        const originalHeight = mainLineInput.scrollHeight; // baseline height
+        // check for characters surpassing first line | create new terminal line
+        // if so
+        mainLineInput.addEventListener("input", (event) => {
+            var _a;
+            event.preventDefault();
+            // check for changes in rows
+            if (mainLineInput.scrollHeight > originalHeight) {
+                const nextInput = document.createElement("textarea");
+                nextInput.id = "focus-input";
+                commandLine === null || commandLine === void 0 ? void 0 : commandLine.appendChild(nextInput);
+                // remove focus id
+                mainLineInput.id = "";
+                (_a = document.getElementById("focus-input")) === null || _a === void 0 ? void 0 : _a.focus();
+                alterTextHeight();
+            }
+        });
+    }
 }
--- scripts/resizeTermInput.ts
+++ scripts/resizeTermInput.ts
@@ -1,12 +1,37 @@
-function alterTextAreaSize() {
-    document.querySelectorAll("textarea").forEach((textarea) => {
-        textarea.style.height = textarea.scrollHeight+"px";
-        textarea.style.overflowY = "hidden";
+function alterTextHeight() {
+    const inputLine = document.getElementById("focus-input");
 
-        textarea.addEventListener("input", function (this: HTMLElement) {
+    if (inputLine instanceof HTMLTextAreaElement) {
+        inputLine.style.height = inputLine.scrollHeight+"px";
+
+        inputLine.addEventListener("input", function (this: HTMLElement) {
             this.style.height = "auto";
             this.style.height = this.scrollHeight + "px";
-            console.info("helpp");
         })
-    });
+    }
+}
+
+function terminalNewLine() {
+    const commandLine = document.getElementById("terminal--line"); // parent container
+    const mainLineInput = document.getElementById("focus-input"); // first input line
+
+    if (mainLineInput instanceof HTMLTextAreaElement) {
+        const originalHeight = mainLineInput.scrollHeight; // baseline height
+        
+        // check for characters surpassing first line | create new terminal line
+        // if so
+        mainLineInput.addEventListener("input", (event) => {
+            event.preventDefault();
+            // check for changes in rows
+            if (mainLineInput.scrollHeight > originalHeight) {
+                const nextInput = document.createElement("textarea");
+                nextInput.id = "focus-input";
+                commandLine?.appendChild(nextInput);
+                // remove focus id
+                mainLineInput.id = "";
+                document.getElementById("focus-input")?.focus();
+                alterTextHeight();
+            }
+        });
+    }
 }
