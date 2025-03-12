71c8c960d6491abf34049e36490e867192c5b959
Levi-McKenna <lmckenna090@gmail.com>
2025-03-11 23:55:20 -0400

    command parsing is semi-functional now

--- dist/input.js
+++ dist/input.js
@@ -1,12 +1,11 @@
 "use strict";
-const inputIdName = "inputFocus";
 /**
  * Controls keyboard input for mouse and keyboard. Website will need a separate
  * mobile friendly web page.
  */
 function addInput() {
     document.addEventListener("keydown", (event) => {
-        const inputLine = document.getElementById(inputIdName);
+        const inputLine = document.getElementById(INPUT_ID);
         if (!inputLine)
             return;
         console.log(event.key);
@@ -30,6 +29,7 @@ function addInput() {
                 break;
             case "Enter":
                 // create new line and pass command to interpreter
+                parseCommand(getCommand());
                 newPrompt();
                 break;
             default:
@@ -37,4 +37,78 @@ function addInput() {
                 break;
         }
     });
+}
+/**
+ * Returns the command currently stored in the user input element.
+ *
+ * @returns {string} textContent of the input element or an empty string if the
+ * content is empty or the element does not exist
+ */
+function getCommand() {
+    const inputElement = document.getElementById(INPUT_ID);
+    if (inputElement) {
+        return inputElement.textContent ? inputElement.textContent : "";
+    }
+    return "";
+}
+/**
+ * Parse a command to its underlying program, options an arguments
+ *
+ * @param {string} command - full command from user input
+ */
+function parseCommand(command) {
+    let program = "";
+    let options = [];
+    let args = [];
+    let start = 0;
+    let stringLiteral = false;
+    let optionLiteral = false;
+    for (let i = 0; i < command.length; i++) {
+        if ((command[i] == " " || i == command.length - 1)
+            && !stringLiteral) {
+            if (start == i)
+                continue; // ensure not parsing an empty slice
+            if (optionLiteral) { // prevent parsing options as args
+                optionLiteral = false;
+                start = i + 1;
+                continue;
+            }
+            // program must be the first word
+            if (start == 0) {
+                program = command.slice(0, i == command.length - 1 ? i + 1 : i);
+                start = i + 1;
+                continue;
+            }
+            args.push(command.slice(start, i == command.length - 1 ? i + 1 : i));
+            start = i + 1;
+        }
+        else if ((command[i] == `"` && !optionLiteral) ||
+            i == command.length - 1) { // indicate literal parsing
+            // inverse whether a literal is being parsed
+            stringLiteral = !stringLiteral;
+            if (stringLiteral)
+                start = i + 1; // start the arg at the beginning of a string delimiter
+            else if (command[i] == `"`) { // if the character truly is the string delimiter then remove it
+                args.push(command.slice(start, i));
+                start = i + 1;
+            }
+            else
+                args.push(command.slice(start, i + 1)); // else we parse the argument to the end of the command
+            continue;
+        }
+        else if (command[i] == "-" && !stringLiteral) {
+            optionLiteral = true;
+            continue;
+        }
+        // when reading a list of options/flags
+        if (optionLiteral) {
+            options.push(command[i]);
+            i++;
+            start = i;
+            continue;
+        }
+    }
+    console.log("Program : " + program);
+    console.log("Args : " + args);
+    console.log("Options : " + options);
 }
